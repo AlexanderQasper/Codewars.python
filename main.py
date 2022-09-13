9234566ab90060164737940f728888de7b2f7656
Alex QAsper <semidog@gmail.com>
2022-09-13 20:03:29 +0300

    Enought is enought

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # Unique In Order
-def unique_in_order(iterable):
-    newList = []
-    for item in iterable:
-        if len(newList) < 1 or not item == newList[len(newList) - 1]:
-            newList.append(item)
-    return newList
+# def unique_in_order(iterable):
+#     newList = []
+#     for item in iterable:
+#         if len(newList) < 1 or not item == newList[len(newList) - 1]:
+#             newList.append(item)
+#     return newList
+#
+# print(unique_in_order('A'))
 
-print(unique_in_order('A'))
+# Enough is enough!
 
+list1 = [1, 1, 3, 3, 7, 2, 2, 2, 2]
+numb = 3
+
+
+def delete_nth(order, max_e):
+    result = []
+    occurrences = {}
+
+    for n in order:
+        count = occurrences.setdefault(n, 0)
+        if count >= max_e:
+            continue
+        result.append(n)
+        occurrences[n] += 1
+
+    return result
